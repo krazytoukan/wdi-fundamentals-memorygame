c99ac4309d9bca32dd7601932631ff8f674603ad
John Cambou <johncambou@Johns-MBP.attlocal.net>
2018-04-04 15:44:01 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+console.log("Player Flipper " + cardOne);
+console.log("Player Flipper " + cardFour);

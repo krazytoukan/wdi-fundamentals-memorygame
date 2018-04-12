7e9d760372235911c2d1f14d3766b61d2d394287
John Cambou <johncambou@Johns-MBP.attlocal.net>
2018-04-12 12:27:00 -0700

    created game logic from unit 10

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,27 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -10,16 +32,18 @@ var checkForMatch = function() {
 	} else {
 		console.log("You have failed miserably.");
 	}
-
-}
+};
 
 var flipCard = function(cardId){
-	console.log("Player flipped " + cards[cardId] + ".");
-	cardsInPlay.push(cards[cardId]);
+	console.log("Player flipped " + cards[cardId].rank + ".");
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 	checkForMatch();
-}
+};
+
 flipCard(0);
-flipCard(2);
+flipCard(3);
 
 
 
